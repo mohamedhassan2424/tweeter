79425e3cab8bb105a6baa9a2a5ddb275f375e783
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-10-02 13:10:44 +0000

    fixing my tweeter code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,29 @@
-$(document).ready(function(event) {
-    // --- our code goes here ---
-    //console.log("Hello World #tweet-text")
-    $(".textareaclass").on('keypress', (event) =>{
-        //console.log("HelloWorld")
-        const newTweet = event.target.value.length
-        const newValue =140 - newTweet
-        $('.counter').val(newValue)
-        //console.log(event)
-        //console.log(newTweet)
-    })
-    
-    //const lengthInput = newTweet.on("change", function(event){
-     //   event.length
-
-  });
-  $(document).on("blur", function(event){
-    //console.log(event)
-  })
-
+ $(document).ready(function(event) {
+          // --- our code goes here ---
+          //console.log("Hello World #tweet-text")
+          $(".textareaclass").on('keypress', (event) =>{
+              //console.log("HelloWorld")
+              const newTweet = event.target.value.length
+              const newValue =140 - newTweet
+              $('.counter').val(newValue)
+              console.log(newTweet)
+              console.log(newTweet<0)
+              if(newTweet<0){
+                $('.counter').addClass("warning-text");
+              }else{
+                $('.counter').removeClass("warning-text");
+              }
+              //console.log(event)
+              //console.log(newTweet)
+          })
+          
+          //const lengthInput = newTweet.on("change", function(event){
+           //   event.length
+      
+        });
+        /*$(document).on("blur", function(event){
+          //console.log(event)
+        })*/
 
 
 
